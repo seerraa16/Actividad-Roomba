c09600f607d4578f65d7fb95aafe2c9729644c1a
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2024-02-11 12:07:50 +0100

    terminado calculo

--- Calculo.py
+++ Calculo.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
 
-class RobotAspirador:
+class roomba:
     def __init__(self, velocidad_limite=1.0, rendimiento=0.5):
-        self.velocidad_limite = velocidad_limite  # en m/s
-        self.rendimiento = rendimiento  # en m^2/s
+        self.velocidad_limite = velocidad_limite 
+        self.rendimiento = rendimiento  
 
     def calcular_superficie(self, largo, ancho):
-        return largo * ancho  # Área de la habitación en metros cuadrados
+        return largo * ancho  
 
     def estimar_tiempo_limpieza(self, area_habitacion, area_obstaculo):
         tiempo = (area_habitacion - area_obstaculo) / self.rendimiento
@@ -20,11 +20,9 @@ def calcular_y_mostrar_resultados():
         largo_obstaculo = float(entry_largo_obstaculo.get())
         ancho_obstaculo = float(entry_ancho_obstaculo.get())
         
-        # Calcular la superficie de la habitación
         area_habitacion = mi_robot.calcular_superficie(largo, ancho)
         area_obstaculo = mi_robot.calcular_superficie(largo_obstaculo, ancho_obstaculo)
         
-        # Estimar el tiempo de limpieza
         tiempo_limpieza = mi_robot.estimar_tiempo_limpieza(area_habitacion, area_obstaculo)
         
         texto_resultado = f"Superficie de la habitación: {area_habitacion:.2f} metros cuadrados\n"
@@ -35,14 +33,10 @@ def calcular_y_mostrar_resultados():
     except ValueError:
         messagebox.showerror("Error", "Por favor, ingresa valores numéricos para las dimensiones.")
 
-# Crear la ventana principal
 root = tk.Tk()
 root.title("Calculadora de Superficie y Tiempo de Limpieza")
 
-# Crear un objeto RobotAspirador
-mi_robot = RobotAspirador()
-
-# Crear y colocar los widgets en la ventana
+mi_robot = roomba()
 frame = tk.Frame(root)
 frame.pack(padx=20, pady=10)
 
@@ -73,6 +67,5 @@ entry_ancho_obstaculo.grid(row=3, column=1, padx=5, pady=5)
 boton_calcular = tk.Button(frame, text="Calcular", command=calcular_y_mostrar_resultados)
 boton_calcular.grid(row=4, columnspan=2, padx=5, pady=10)
 
-# Ejecutar la aplicación
 root.mainloop()
 
